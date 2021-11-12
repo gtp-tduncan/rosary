3353f285fb86e0612e31c33b8f528aafc590e7a2
gtp-tduncan <gtp.tduncan@gmail.com>
2021-11-11 21:02:31 -0600

    WIP - attempt #1 full screen

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, Input, OnInit, ViewChild } from '@angular/core';
 import { BeadGroupLoaderService } from '../../services/bead-group-loader.service';
 import { RosaryMysteriesEnum } from '../../sequences/rosary-helper';
 import { BeadGroupList } from '../../models/bead-group-list';
@@ -23,9 +24,12 @@ export class HomeComponent implements OnInit {
   @ViewChild(ActivePrayerComponent)
   activePrayer: ActivePrayerComponent;
 
+  elem: any;
+
   constructor(private beadGroupLoader: BeadGroupLoaderService,
               public appConfig: AppConfigService,
-              private liturgicalYear: LiturgicalYearService) {
+              private liturgicalYear: LiturgicalYearService,
+              @Inject(DOCUMENT) private document: any) {
   }
 
   ngOnInit(): void {
@@ -33,6 +37,8 @@ export class HomeComponent implements OnInit {
     if (this.selectedMysteryId) {
       this.onMysterySelected(RosaryMysteriesEnum[this.selectedMysteryId]);
     }
+    this.elem = document.documentElement;
+    this.openFullscreen();
   }
 
   onMysterySelected(selectedMystery: RosaryMysteriesEnum): void {
@@ -74,4 +80,34 @@ export class HomeComponent implements OnInit {
     }
   }
 
+  openFullscreen() {
+    if (this.elem.requestFullscreen) {
+      this.elem.requestFullscreen();
+    } else if (this.elem.mozRequestFullScreen) {
+      /* Firefox */
+      this.elem.mozRequestFullScreen();
+    } else if (this.elem.webkitRequestFullscreen) {
+      /* Chrome, Safari and Opera */
+      this.elem.webkitRequestFullscreen();
+    } else if (this.elem.msRequestFullscreen) {
+      /* IE/Edge */
+      this.elem.msRequestFullscreen();
+    }
+  }
+
+  /* Close fullscreen */
+  closeFullscreen() {
+    if (this.document.exitFullscreen) {
+      this.document.exitFullscreen();
+    } else if (this.document.mozCancelFullScreen) {
+      /* Firefox */
+      this.document.mozCancelFullScreen();
+    } else if (this.document.webkitExitFullscreen) {
+      /* Chrome, Safari and Opera */
+      this.document.webkitExitFullscreen();
+    } else if (this.document.msExitFullscreen) {
+      /* IE/Edge */
+      this.document.msExitFullscreen();
+    }
+  }
 }
