dd74a1cad77d61898923923a277c172ed7ac5b57
gtp-tduncan-samsung <gtp.tduncan@gmail.com>
2021-11-26 13:41:09 -0600

    Possible fix language selection url

--- src/app/components/language-redirect/language-redirect.component.ts
+++ src/app/components/language-redirect/language-redirect.component.ts
@@ -27,8 +27,9 @@ export class LanguageRedirectComponent implements OnInit {
 
   ngOnInit(): void {
     if (this.supportedLanguages.indexOf(this.language) >= 0 && !this.matchUrlEnd(window.location.href, this.language)) {
+      const rootUrl = window.location.href.substring(0, window.location.href.length - 3);
       console.log(`original url: ${window.location.href}`);
-      window.location.href = `/${this.language}/`;
+      window.location.href = `${rootUrl}/${this.language}/`;
       console.log(`updated url: ${window.location.href}`);
     }
     console.log(`using url: ${window.location.href}`);
